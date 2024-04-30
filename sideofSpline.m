9522bcb4a17d2552e1ca58358170ce5c180608cb
bryanlailey <55509261+bryanlailey@users.noreply.github.com>
2024-04-30 10:04:46 -0400

    Added offset term as an input
    Added mag_shift as an input which let's you specify a horizontal shift (in magnitudes) to explore how the distribution of accepted and rejected be stars changes with shift in a Milone 2018 type Halpha emitter plot.

--- sideofSpline.m
+++ sideofSpline.m
@@ -3,11 +3,12 @@
 % linearization of the spline with the same y-value as the point and
 % a line connecting the point with the linearization. The sign of this quantity 
 % can be compared to a known point to determine which side of the spline 
-% the points lie on.
+% the points lie on. Magshift shifts the spline horizontally to enable
+% Ha detection via the method of Milone2018.
 
-function det = sideofSpline(bex,bey,spline);
+function det = sideofSpline(bex,bey,spline,mag_shift);
 
-det = (bex - (slmsolve(spline,bey-0.005)-0.15))*(ppval(spline,slmsolve(spline,bey+0.005)) - ppval(spline,slmsolve(spline,bey-0.005)))...
-    - (bey - ppval(spline,slmsolve(spline,bey-0.005)))*((slmsolve(spline,bey+0.005)-0.15) - (slmsolve(spline,bey-0.005)-0.15));
+det = (bex - (slmsolve(spline,bey-0.005)+mag_shift))*(ppval(spline,slmsolve(spline,bey+0.005)) - ppval(spline,slmsolve(spline,bey-0.005)))...
+    - (bey - ppval(spline,slmsolve(spline,bey-0.005)))*((slmsolve(spline,bey+0.005)+mag_shift) - (slmsolve(spline,bey-0.005)+mag_shift));
 
 end
